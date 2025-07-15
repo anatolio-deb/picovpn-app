488a4eff912d99c462ae2bb56279303a60f1a608
anatolio-deb <nikiforova693@gmail.com>
2025-07-15 12:33:44 +0000

    vue tg

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <v-app>
     <router-view />
   </v-app>
@@ -6,4 +6,15 @@
 
 <script lang="ts" setup>
   //
+</script> -->
+
+<template>
+  <MainButton text="Open alert" @click="() => popup.showAlert('Hello!')" />
+</template>
+
+<script lang="ts" setup>
+import { MainButton } from 'vue-tg'
+import { usePopup } from 'vue-tg/latest'
+
+const popup = usePopup()
 </script>
